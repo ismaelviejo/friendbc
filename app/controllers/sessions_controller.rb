7ac1b773c19357ebf2e1e8cb2829e525ed87c363
Ismael Viejo <ismaelviejo@gmail.com>
2013-08-20 17:32:01 -0500

    sign-in basic logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ def new
 end
 
 def create
+  @user = User.find_by username: params[:username]
+
+  if @user.present? && @user.authentication(params[:password])
+    session[:user_id] = @user.id
+    redirect_to root_url
+  else
+    redirect_to new_session_url
 
 end
 
